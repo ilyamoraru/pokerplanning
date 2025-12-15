14f982154ba30dd603106ca100cd12b77d23afe4
Moraru Ilya <i.moraru@intickets.ru>
2025-12-15 18:59:45 +0300

    feature переписал работу с сокетом на плагин накста. начал работу со страницей команты

--- app/components/task/slideover.vue
+++ app/components/task/slideover.vue
@@ -5,11 +5,40 @@
     </UButton>
 
     <template #body>
-      <TaskEstimateList v-if="tasksToEstimate" :value="tasksToEstimate" />
+      <template v-if="tasksToEstimate">
+        <header class="py-4 bg-white sticky -top-6 z-10">
+          <UInput v-model="search" size="xl" placeholder="Поиск задачи" class="w-full">
+            <template v-if="search.length" #trailing>
+              <UButton
+                color="neutral"
+                variant="link"
+                size="sm"
+                icon="mdi:close"
+                @click="search = ''"
+              />
+            </template>
+          </UInput>
+        </header>
+        <TaskEstimateList v-if="tasksToDisplay.length" :value="tasksToDisplay" />
+        <UEmpty v-else variant="naked" title="Задачи не найдены" class="mt-1"></UEmpty>
+      </template>
     </template>
   </USlideover>
 </template>
 
 <script setup lang="ts">
 const { tasksToEstimate } = storeToRefs(useTaskStore())
+
+const search = ref('')
+const tasksToDisplay = computed(() => {
+  if (!search.value) return tasksToEstimate.value ?? []
+
+  return (
+    tasksToEstimate.value?.filter(
+      (item) =>
+        item.id.toLowerCase().includes(search.value.toLowerCase()) ||
+        item.title.toLowerCase().includes(search.value.toLowerCase())
+    ) ?? []
+  )
+})
 </script>
