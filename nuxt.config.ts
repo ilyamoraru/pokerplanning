0ccae03028f3f300dc5e82f2e0aa60df017671c3
Moraru Ilya <i.moraru@intickets.ru>
2025-12-19 15:43:39 +0300

    feature apiBaseUrl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,5 +27,10 @@ export default defineNuxtConfig({
     serverBundle: {
       collections: ['mdi', 'lucide']
     }
+  },
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || '/api'
+    }
   }
 })
